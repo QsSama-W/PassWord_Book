7b9cda2a9c9544f4595aefecdec9c37383c266c8
QsSama-W <136866021+QsSama-W@users.noreply.github.com>
2025-02-04 15:59:33 +0800

    Update login.js

--- static/js/login.js
+++ static/js/login.js
@@ -64,3 +64,15 @@ function startCountdown(remainingTime) {
         }
     }, 1000);
 }
+
+// 超时退出提示框
+document.addEventListener('DOMContentLoaded', function () {
+    const urlParams = new URLSearchParams(window.location.search);
+    const timeout = urlParams.get('timeout');
+    if (timeout === 'true') {
+        const popupMessage = document.getElementById('popup-message');
+        popupMessage.textContent = '长时间无动作已退出登录';
+        // 调用showPopup函数显示弹窗
+        showPopup('长时间无动作已退出登录');
+    }
+});
