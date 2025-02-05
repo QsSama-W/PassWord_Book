05aedea6e3c2ee5faadabc2f9c034d21cd532671
QsSama-W <136866021+QsSama-W@users.noreply.github.com>
2025-02-05 23:15:20 +0800

    Update register.php

--- register.php
+++ register.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION['csrf_token'])) {
 
 // 检查用户是否已登录
 if (isset($_SESSION["user_id"])) {
-    require_once 'db_connection.php';
+    require_once '#db_connection.php';
     
     $stmt_config = $conn->prepare("SELECT value FROM config WHERE key_name = 'allowed_roles'");
     $stmt_config->execute();
@@ -35,11 +35,69 @@ if (isset($_SESSION["user_id"])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>用户注册</title>
+    <link rel="icon" type="image/png" href="./static/img/logo.png">
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f9;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+
+        h2 {
+            color: #333;
+            text-align: center;
+            margin-bottom: 20px;
+        }
+
+        form {
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            width: 300px;
+        }
+
+        label {
+            display: block;
+            margin-bottom: 5px;
+            color: #666;
+        }
+
+        input[type="text"],
+        input[type="password"] {
+            width: 100%;
+            padding: 10px;
+            margin-bottom: 15px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+        }
+
+        input[type="submit"] {
+            width: 100%;
+            padding: 10px;
+            background-color: #007BFF;
+            color: #fff;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+            transition: background-color 0.3s ease;
+        }
+
+        input[type="submit"]:hover {
+            background-color: #0056b3;
+        }
+    </style>
 </head>
 
 <body>
-    <h2>用户注册</h2>
+    
     <form action="register_process.php" method="post">
+        <h2>用户注册</h2>
         <input type="hidden" name="csrf_token" value="<?php echo $_SESSION['csrf_token']; ?>">
         <label for="username">用户名:</label>
         <input type="text" id="username" name="username" required>
