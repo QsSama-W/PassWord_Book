021490a7694f8fa4020bfa718e70c7f23cd7b8e9
QsSama-W <136866021+QsSama-W@users.noreply.github.com>
2025-02-05 21:08:46 +0800

    Update save_note.php

--- save_note.php
+++ save_note.php
@@ -4,21 +4,23 @@ if (!isset($_SESSION["user_id"])) {
     header("Location: login.html");
     exit();
 }
-// 引入数据库连接文件
 require_once 'db_connection.php';
 
 $title = $_POST["title"];
 $content = $_POST["content"];
 $content2 = $_POST["content2"];
 $content3 = $_POST["content3"];
+$user_id = $_SESSION["user_id"];
 
-$sql = "INSERT INTO notes (title, content, content2, content3) VALUES ('$title', '$content', '$content2', '$content3')";
-
-if ($conn->query($sql) === TRUE) {
+$sql = "INSERT INTO notes (title, content, content2, content3, user_id) VALUES (?,?,?,?,?)";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("ssssi", $title, $content, $content2, $content3, $user_id);
+if ($stmt->execute()) {
     header("Location: notepad.php");
+    exit();
 } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+    echo "保存失败：" . $conn->error;
 }
-
+$stmt->close();
 $conn->close();
 ?>
